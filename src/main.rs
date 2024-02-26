c460b43f25156f53304b81dc0c447f0a7791aa7c
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-02-26 23:04:05 +0600

    add primitive types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+fn basic_print() {
+    let x: i32 = 128;
+    let y = 256;
+    let mut z: f32 = 512.12;
     println!("Hello, world!");
+    println!("{}", x);
+    println!("{}", y);
+    println!("{}", z);
+
+    z = 1024.24;
+    println!("{}", z);
+}
+
+fn main() {
+    basic_print()
 }
