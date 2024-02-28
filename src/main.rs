5cac0024e1f2fba255fb8b35708c52784d4e2b1a
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-02-28 23:35:09 +0600

    add module

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-fn basic_print() {
-    let x: i32 = 128;
-    let y = 256;
-    let mut z: f32 = 512.12;
-    let s = "str";
-    println!("Hello, world!");
-    println!("{}", x);
-    println!("{}", y);
-    println!("{}", z);
-    println!("{}", s);
-
-    z = 1024.24;
-    println!("{}", z);
-}
+mod primitives;
 
 fn main() {
-    basic_print()
+    primitives::basic_print()
 }
