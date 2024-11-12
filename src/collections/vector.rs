c73483572e1b8f965b4b573ea68cb871f39091fa
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-11-13 00:28:44 +0600

    add vector examples

--- src/collections/vector.rs
+++ src/collections/vector.rs
@@ -47,4 +47,24 @@ pub fn vectors() {
         *x *= 3;
     }
     println!("Updated vector: {:?}", xs);
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("vector v: {:?}", v);
+
+    let v2 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v2[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v2.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
