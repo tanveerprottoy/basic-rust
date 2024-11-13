d9abc8d7670f05a03a6891c66f76dff6306c25cd
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-11-14 00:23:06 +0600

    add vector examples

--- src/collections/vector.rs
+++ src/collections/vector.rs
@@ -68,3 +68,39 @@ pub fn vectors() {
         None => println!("There is no third element."),
     }
 }
+
+pub fn mutable_immutable_mix_error() {
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let first = &v[0];
+
+    v.push(6);
+
+    println!("The first element is: {first}");
+}
+
+pub fn iterate() {
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+}
+
+pub fn vector_of_enums() {
+     enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
