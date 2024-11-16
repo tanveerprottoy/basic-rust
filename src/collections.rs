aed9b8ee55937f1f55ff993d7a54c54d15307b72
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-11-17 00:20:33 +0600

    add string code

--- src/collections.rs
+++ src/collections.rs
@@ -1,1 +1,3 @@
 pub mod vector;
+
+pub mod string;
---
+++ src/collections/string.rs
@@ -0,0 +1,22 @@
+pub fn string() {
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
+
+    let hello = String::from("Hello");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+}
