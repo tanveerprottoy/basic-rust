8a76da0c1adf110af1ce0c30d1cae27ffb338c85
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-11-22 00:17:00 +0600

    add hashmap

---
+++ src/collections/hashmap.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+pub fn hash_maps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
+}
