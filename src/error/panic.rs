6a9b7433f999ab9d9cb5a8de015717759f7aa8e2
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-11-27 22:56:09 +0600

    add error code

---
+++ src/error/panic.rs
@@ -0,0 +1,7 @@
+pub fn unrecoverable_panic() {
+    panic!("crash and burn");
+
+    let v = vec![1, 2, 3];
+
+    v[99];
+}
---
+++ src/error/result.rs
@@ -0,0 +1,3 @@
+pub fn result() {
+    let greeting_file_result = File::open("hello.txt");
+}
