e50e6738bf7bb72af729211a3464fad312779c26
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-12-07 11:22:20 +0600

    add error code

--- src/error/result.rs
+++ src/error/result.rs
@@ -1,3 +1,9 @@
 pub fn result() {
     let greeting_file_result = File::open("hello.txt");
+
+    
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
 }
